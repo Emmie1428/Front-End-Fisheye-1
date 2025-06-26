442a458ffedbeb08790b59976658ef5ead4d3bd4
Emmie Gemme <mimiemmie@gmail.com>
2025-06-26 18:33:18 -0400

    key.event de la modal terminés

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,44 +1,58 @@
-let modalOpener;
+let firstElement = null;
+let lastElement = null;
 
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+	const opacity = document.getElementById("modal_opacity");
+
+    modal.style.display = "block";
     modal.setAttribute("aria-hidden", "false");
+    opacity.style.display = "block";
 
     //Retirer le focus de l'arrière-plan
     const main = document.getElementById("main");
     main.setAttribute("aria-hidden", "true");
-
-    modalOpener = document.activeElement;
     
     const focusSeletors = "input, textarea, button";
     const  focusElements = modal.querySelectorAll (focusSeletors);
-    const firstElement = focusElements[0];
-    const lastElement = focusElements[focusElements.length -1];
+    
+    //Pour coincer le focus dans la modal
+    firstElement = focusElements[0];
+    lastElement = focusElements[focusElements.length -1];
 
     if (firstElement) firstElement.focus(); 
-
-    modal.addEventListener("keydown", function (event) {
-        if (event.key === "Tab" && document.activeElement === lastElement) {
-            event.preventDefault(); 
-            firstElement.focus();   
-        } 
-        if(event.key ==="Escape") {
-        closeModal();
-        }
-    });
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const opacity = document.getElementById("modal_opacity");
     modal.style.display = "none";
     modal.setAttribute("aria-hidden", "true");
+    opacity.style.display = "none";
 
     //Retirer le focus du formulaire
     const main = document.getElementById("main");
     main.setAttribute("aria-hidden", "false");
-
-    if (modalOpener) {
-        modalOpener.focus();
-    }
 }
+
+//Permet de manipuler la modal avec tab et escape
+document.addEventListener("keydown", function (event) {
+        const modal = document.getElementById("contact_modal")
+        const visible = modal && modal.style.display === "block";
+
+        if (!visible) return; 
+        
+        if (event.key === "Tab") {
+            if (event.shiftKey && document.activeElement === firstElement) {
+            event.preventDefault(); 
+            lastElement.focus();  
+        } else if (!event.shiftKey && document.activeElement === lastElement) {
+            event.preventDefault(); 
+            firstElement.focus();   
+        } 
+    }
+        
+        if(event.key ==="Escape") {
+        closeModal();
+        }
+    });
